3ac6cb26abeb11ff457cf78a8f862fd1e14be9f9
NewtonGitau <newtongitau.nm@gmail.com>
2022-01-03 16:15:45 +0300

    Add styles to paragraphs.

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("input#message").css({
+        width: "80%",
+        padding: "12px",
+        margin: "8px",
+        height: "500px",              
+    })
+    $("input").css({
+        border: "solid white",
+    })
+})
