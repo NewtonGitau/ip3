615122807f13fcfb11639a6f3e2374c7ec330bdf
NewtonGitau <95222959+NewtonGitau@users.noreply.github.com>
2022-01-10 06:52:08 +0300

    Update main.js
    Add style to input message box.

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
-    // $("input#message").css({
-    //     width: "80%",
-    //     padding: "12px",
-    //     margin: "8px",
-    //     height: "500px",              
-    // })
+    $("input#message").css({
+        width: "80%",
+        padding: "12px",
+        margin: "8px",
+        height: "500px",              
+    })
     $("input").css({
         border: "solid white",
     })
